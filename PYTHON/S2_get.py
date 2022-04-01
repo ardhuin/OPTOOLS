c559cade488130c335153f6568fbc5db8c605648
Fabrice Ardhuin <fabrice.ardhuin@ifremer.fr>
2022-04-01 17:27:14 +0200

    FA: Starting to add the sun angles in python scripts

--- PYTHON/S2_get.py
+++ PYTHON/S2_get.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from S2_read import *
-from s2_angs import *
-#S2file='/home/ardhuin/ADMIN/PROPOSALS/EE11/S2/DATA/T11SMS_20160429T183252';
-S2file='/home/datawork-WW3/IMAGERY/S2/S2A_MSIL1C_20160429T183252_N0201_R027_T11SMS_20160429T184037.SAFE/GRANULE/L1C_T11SMS_A004457_20160429T184037/IMG_DATA/T11SMS_20160429T183252'
-boxi=[5600,6400,3600,4400] # East West South North 
+from s2_and_sun_angs import *
+S2file='/home/ardhuin/ADMIN/PROPOSALS/EE11/S2/DATA/T11SMS_20160429T183252';
+#S2file='/home/datawork-WW3/IMAGERY/S2/S2A_MSIL1C_20160429T183252_N0201_R027_T11SMS_20160429T184037.SAFE/GRANULE/L1C_T11SMS_A004457_20160429T184037/IMG_DATA/T11SMS_20160429T183252'
+boxi=[5600,6400,3600,4400] # bounding box indices in image:  East West South North 
 bands=['B04','B03','B02'];  
 nb=np.shape(bands)[0]
 [imgs,NX,NY,nx,ny,dx,dy]=S2_read(S2file,boxi,bands)
@@ -12,23 +12,29 @@ nb=np.shape(bands)[0]
 # defines indices of where we are in coarse array of angles
 indx=int(np.round((NX*10/dx-0.5*(boxi[0]+boxi[1]))/(5000/dx)))
 indy=int(np.round((NY*10/dx-0.5*(boxi[2]+boxi[3]))/(5000/dx)))
-S2xml='/home/datawork-WW3/IMAGERY/S2/S2A_MSIL1C_20160429T183252_N0201_R027_T11SMS_20160429T184037.SAFE/GRANULE/L1C_T11SMS_A004457_20160429T184037/MTD_TL.xml'
-(Tile_ID, AngleObs) = get_angleobs(S2file+'.xml')
+S2xml=S2file+'.xml'
+
+#S2xml='/home/datawork-WW3/IMAGERY/S2/S2A_MSIL1C_20160429T183252_N0201_R027_T11SMS_20160429T184037.SAFE/GRANULE/L1C_T11SMS_A004457_20160429T184037/MTD_TL.xml'
+
+(Tile_ID, AngleObs, AngleSun) = get_angleobs(S2xml)
 nrows=AngleObs['nrows']
 ncols=AngleObs['ncols']
 ulx=AngleObs['ul_x']
 uly=AngleObs['ul_y']
 nz1=nz2=23
 satg = numpy.zeros(shape=(nz1,nz2,13,3))
+sung = numpy.zeros(shape=(nz1,nz2,5))
 detector=numpy.zeros(shape=(nz1,nz2,13))
 
 observe=AngleObs['obs']
+sunall=AngleSun['sun']
 sh1=np.shape(observe)
 ncells=sh1[0]
 
 for icell in range(0,ncells):
 #  This takes the nearest cell in matrix 
    thiscell=observe[icell]
+   suncell=sunall[icell]
    iband=thiscell[0]
    ix=int((thiscell[2]-ulx)/5000)
    iy=int((thiscell[3]-uly)/5000)
@@ -36,7 +42,16 @@ for icell in range(0,ncells):
    satg[ix,iy,iband,0]=thiscell[4][0]
    satg[ix,iy,iband,1]=thiscell[4][1]
    satg[ix,iy,iband,2]=thiscell[4][2]
-   
+
+for isun in range(0,nz1*nz2):
+   suncell=sunall[isun]
+   ix=int((suncell[0]-ulx)/5000)
+   iy=int((suncell[1]-uly)/5000)
+   sung[ix,iy,0]=suncell[2]
+   sung[ix,iy,1]=suncell[3]
+   sung[ix,iy,2]=suncell[4][0]
+   sung[ix,iy,3]=suncell[4][1]
+   sung[ix,iy,4]=suncell[4][2]
 
 
 #observe = [bandId, detectorId, xcoord, ycoord, Sat, Gx]
