a3494980fd41143d775b9db69d2b2824bef6ad95
Fabrice Ardhuin <fabrice.ardhuin@ifremer.fr>
2022-05-12 09:54:09 +0200

    Added notebook with envelope spectra in relation to effects of groups on altimeter data

--- PYTHON/simulator_functions.py
+++ PYTHON/simulator_functions.py
@@ -5,6 +5,7 @@
 # ==================================================================================
 from wave_physics_functions import *
 import scipy.special as sps # function erf
+import scipy.interpolate as spi # function griddata
 import numpy as np
 
 
@@ -66,6 +67,72 @@ def surface_from_Z1kxky(Z1,kX,kY,nx=None,ny=None,dx=None,dy=None,dkx=None,dky=No
 	
 	return S1,X,Y 
 	
+
+
+def surface_from_Z1kxky_uniform_phase(Z1,kX,kY,i,nx=None,ny=None,dx=None,dy=None,dkx=None,dky=None):
+    # /!\ Watch out : shape(S) = (ny,nx)
+    # usually when doing X,Y=np.meshgrid(x,y) with size(x)=nx and size(y)=ny => size(X)=size(Y)= (ny,nx)
+    kX0 = np.unique(kX)
+    kY0 = np.unique(kY)
+    if nx==None:
+        nx = Z1.shape[1]
+    if ny==None:
+        ny = Z1.shape[0]
+    shx = np.floor(nx/2-1)
+    shy = np.floor(ny/2-1)
+    if (dx==None):
+        if dkx == None:
+            dx = 2*np.pi/((kX0[1] - kX0[0])*nx)
+        else:
+            dx = 2*np.pi/(dkx*nx)
+
+    if (dy==None):
+        if dky == None:
+            dy = np.floor(2*np.pi/((kY0[1] - kY0[0])*ny))
+        else:
+            dy = 2*np.pi/(dky*ny)
+
+    if (dkx==None):
+        dkx = 2*np.pi/(dx*nx)
+    if (dky==None):
+        dky = 2*np.pi/(dy*ny)
+    ########################################################################################
+    # SIDE NOTE :                                                                          #
+    # obtain dkx, dky from dx and dy in order to account for eventual rounding of np.pi	   #
+    # considering that dkx has been defined according to the surface requisite (nx,dx)     #
+    # Eg:	                                                                           #
+    # # initialisation to compute spectrum                                                 #
+    # nx = 205										#
+    # dx = 10										#
+    # dkx = 2*np.pi/(dx*nx)								#
+    # kX0 = dkx*np.arange(-nx//2+1,nx//2+1)						#
+    # 											#
+    # # Compute from kX0 and nx (found from kX0.shape)					#
+    # dkx2 = kX0[1] - kX0[0]								#
+    # dxbis = (2*np.pi/(dkx2*nx)                                                           #
+    # dkx3 = 2*np.pi/(dxbis*nx)								#
+    #											#
+    # print('dkx = ',dkx)     		=> 0.0030649684425266273			#
+    # print('dkx2 = ',dkx2) 		=> 0.0030649684425266277			#
+    # print('dkx3 = ',dkx3) 		=> 0.0030649684425266273                           #
+    #                                                                                      #
+    ########################################################################################
+    rng = np.random.default_rng(i)
+    rg = rng.uniform(low=0.0, high=1.0, size=(ny,nx))
+    zhats=np.fft.ifftshift(np.sqrt(2*Z1*dkx*dky)*np.exp(1j*2*np.pi*rg))#,(-int(shy),-int(shx)),axis=(0,1))
+    ky2D=np.fft.ifftshift(kY)#,(-int(shy),-int(shx)),axis=(0,1)) # checks that ky2D(1,1)=0 ... 
+    kx2D=np.fft.ifftshift(kX)#,(-int(shy),-int(shx)),axis=(0,1)) # checks that kx2D(1,1)=0 ... 
+    
+#     real part
+    S1 = np.real(np.fft.ifft2(zhats,norm="forward"))
+#     also computes imaginary part (useful for envelope calculations) 
+    S2 = np.imag(np.fft.ifft2(zhats,norm="forward"))
+
+    X = np.arange(0,nx*dx,dx) # from 0 to (nx-1)*dx with a dx step
+    Y = np.arange(0,ny*dy,dy)
+
+    return S1,S2,X,Y,kX0,rg,dkx,dky
+##################################################################################
 def def_spectrum_for_surface(nx=2048,ny=2048,dx=10,dy=10,theta_m=30,D=1000,T0=10,Hs=4,sk_theta=0.001,sk_k=0.001,nk=1001,nth=36,klims=(0.0002,0.2),n=4,typeSpec='Gaussian'):
 	dkx = 2*np.pi/(dx*nx)
 	dky = 2*np.pi/(dy*ny)
@@ -95,5 +162,6 @@ def def_spectrum_for_surface(nx=2048,ny=2048,dx=10,dy=10,theta_m=30,D=1000,T0=10
 		print('Hs for Pierson Moskowitz : ',sumZ1)
 
 	return Z1, kX, kY
+
 
 
